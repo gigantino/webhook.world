6b27aa92953f11d22536a67cd1432565639eb8c5
gigantino <giovanni.gigantino@atoms.retexspa.com>
2023-11-25 12:49:27 +0100

    feat: setup shadcn-ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
+import { Button } from "@/components/ui/button";
 import { getServerSession } from "next-auth";
 
 export default async function Home() {
   const session = await getServerSession();
 
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
-      {session ? (
-        <h1 className="text-5xl">Welcome! You are logged in!</h1>
-      ) : (
-        <h1 className="text-5xl">You Shall Not Pass!</h1>
-      )}
-    </div>
+    <>
+      <Button>Click me!</Button>
+      <div className="w-screen h-screen flex items-center justify-center">
+        {session ? (
+          <h1 className="text-5xl">Welcome! You are logged in!</h1>
+        ) : (
+          <h1 className="text-5xl">You Shall Not Pass!</h1>
+        )}
+      </div>
+    </>
   );
 }
