1f7c237ddf5e61ddae4b62a53ba312d8b9971bb9
gigantino <giovanni.gigantino@atoms.retexspa.com>
2023-11-26 23:52:41 +0100

    fix: cookie name

--- lib/getNextAuthCookie.ts
+++ lib/getNextAuthCookie.ts
@@ -1,9 +1,8 @@
 import { env } from "@/env.mjs";
 import { cookies } from "next/headers";
 export default function getNextAuthCookie() {
-  return `${
-    env.IS_PRODUCTION == "yes" ? "__Secure-" : ""
-  }next-auth.session-token=${
-    cookies().get("next-auth.session-token")?.value ?? ""
-  }; Path=/; HttpOnly;`;
+  const cookieName = `${env.IS_PRODUCTION === "yes" ? "__Secure-" : ""
+    }next-auth.session-token`;
+  return `${cookieName}=${cookies().get(cookieName)?.value ?? ""
+    }; Path=/; HttpOnly;`;
 }
