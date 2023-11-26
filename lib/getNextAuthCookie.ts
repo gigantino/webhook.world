e164152016c33b9026e1d58c5e5d912a6b4f36c5
gigantino <giovanni.gigantino@atoms.retexspa.com>
2023-11-26 23:41:28 +0100

    fix: typo

--- lib/getNextAuthCookie.ts
+++ lib/getNextAuthCookie.ts
@@ -2,7 +2,7 @@ import { env } from "@/env.mjs";
 import { cookies } from "next/headers";
 export default function getNextAuthCookie() {
   return `${
-    env.IS_PRODUCTION == "yes" ? "__Secure" : ""
+    env.IS_PRODUCTION == "yes" ? "__Secure-" : ""
   }next-auth.session-token=${
     cookies().get("next-auth.session-token")?.value ?? ""
   }; Path=/; HttpOnly;`;
