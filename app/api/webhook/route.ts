cfae8cc58609f2190d5e2d7c37511331724a298d
gigantino <giovanni.gigantino@atoms.retexspa.com>
2023-11-26 19:05:08 +0100

    feat: add route to get all webhooks

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -5,16 +5,48 @@ import crypto from "node:crypto";
 import { z } from "zod";
 const prisma = new PrismaClient();
 
-const postWebhookSchema = z.object({
-  name: z
-    .string()
-    .min(3, { message: "Name must be at least 3 characters long." })
-    .max(16, { message: "Name must be at most 16 characters long." })
-    .refine((value) => /^[a-zA-Z0-9_-]+$/.test(value), {
-      message:
-        "Invalid name format. It can only contain letters, numbers, underscores, and hyphens.",
-    }),
-});
+export async function GET(request: Request) {
+  const session = await getServerSession();
+  if (!session || !session.user || typeof session.user.image !== "string") {
+    return Response.json(
+      {
+        error: "Unauthorized",
+      },
+      { status: 401 },
+    );
+  }
+
+  const userId = extractGitHubUserId(session.user.image);
+  if (!userId) {
+    return Response.json(
+      {
+        error: "Couldnt't extract GitHub userId",
+      },
+      { status: 500 },
+    );
+  }
+
+  const webhooks = await prisma.webhooks
+    .findMany({
+      where: {
+        author: userId,
+      },
+    })
+    .catch((err) => {
+      console.error("Couldn't fetch webhooks:", err);
+    });
+
+  if (!webhooks) {
+    return Response.json(
+      {
+        error: "Couldnt't fetch webhooks",
+      },
+      { status: 500 },
+    );
+  }
+
+  return Response.json(webhooks);
+}
 
 export async function POST(request: Request) {
   const session = await getServerSession();
@@ -40,6 +72,18 @@ export async function POST(request: Request) {
   const body = await request.json().catch(() => {
     // This method fails if a body isn't returned
   });
+
+  const postWebhookSchema = z.object({
+    name: z
+      .string()
+      .min(3, { message: "Name must be at least 3 characters long." })
+      .max(16, { message: "Name must be at most 16 characters long." })
+      .refine((value) => /^[a-zA-Z0-9_-]+$/.test(value), {
+        message:
+          "Invalid name format. It can only contain letters, numbers, underscores, and hyphens.",
+      }),
+  });
+
   const parsedBody = postWebhookSchema.safeParse(body);
   if (parsedBody.success) {
     const data = parsedBody.data;
